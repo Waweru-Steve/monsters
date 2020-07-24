32034d9b479b260d7f34b611b013fb53f30bcc0e
Waweru-Steve <wawerusteve142@yahoo.com>
2020-07-24 20:05:23 +0300

    Updates

---
+++ precache-manifest.ee7da1960763d53c8f4663fcee6bffbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3de0e02fc663911f0e3f3080c4b2c4e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e93cff2eca6cf74df90",
+    "url": "/monsters-rolodex/static/css/main.cf6ad1a6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e93cff2eca6cf74df90",
+    "url": "/monsters-rolodex/static/js/main.06b16609.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
